74a3060e3b3703128e4760bcfe8670d66e737b1b
Margus Liinev <margus.liinev@gmail.com>
2022-11-21 22:11:55 +0200

    Fixed styling, adding item to the list

--- 01-grocery-bud/main.js
+++ 01-grocery-bud/main.js
@@ -24,6 +24,27 @@ function addItem(e) {
     const value = grocery.value;
     const id = new Date().getTime().toString();
     if (value && !editFlag) {
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+                            <div class="btn-container">
+                                <button type="button" class="edit-btn">
+                                    <i class="fas fa-edit"></i>
+                                </button>
+                                <button type="button" class="delete-btn">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </div>`;
+        list.appendChild(element);
+        displayAlert('item added to the list', 'success');
+        container.classList.add('show-container');
+        // add to local storage
+        addToLocalStorage(id, value);
+        // set back to default
+        setBackToDefault();
     } else if (value && editFlag) {
     } else {
         displayAlert('Please enter value', 'danger');
@@ -36,6 +57,14 @@ function displayAlert(text, action) {
     setTimeout(function () {
         alert.classList.remove(`alert-${action}`);
     }, 1000);
+}
+
+function addToLocalStorage(id, value) {
+    console.log('added to local storage');
+}
+
+function setBackToDefault() {
+    console.log('set back to default');
 }
 
 /* EVENT LISTENERS */
