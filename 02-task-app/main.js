7ce234f377db9305f14db049f408ffdf951726e6
Margus Liinev <margus.liinev@gmail.com>
2022-12-08 22:37:25 +0200

    create a basic task

--- 02-task-app/main.js
+++ 02-task-app/main.js
@@ -5,9 +5,17 @@ const taskBtn = get('.task-btn');
 const closeBtn = get('.close-btn');
 const formCloseBtn = get('.form-close-btn');
 const modal = get('.modal');
+const form = get('.form');
+const title = get('#title');
+const date = get('#date');
+const description = get('#description');
+const taskList = get('.task-list');
+const container = get('.tasks');
 
 /* EDIT VARIABLES */
 /* ==================================================================================================== */
+
+let editFlag = false;
 
 /* FUNCTIONS */
 /* ==================================================================================================== */
@@ -21,12 +29,38 @@ function get(selection) {
     }
 }
 
+function submitForm(e) {
+    e.preventDefault();
+    const titleValue = title.value;
+    const dateValue = date.value;
+    const descriptionValue = description.value;
+    if ((titleValue, dateValue, descriptionValue && !editFlag)) {
+        let element = document.createElement('article');
+        element.classList.add('task');
+        element.innerHTML = `<h5 class="task-title">${titleValue}</h5>
+                             <p class="task-date">${dateValue}</p>
+                             <p class="task-description">${descriptionValue}</p>
+                             <div class="btn-container">
+                                <button type="button" class="edit-btn">
+                                    <i class="fas fa-edit"></i>
+                                </button>
+                                <button type="button" class="delete-btn">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                             </div>`;
+        taskList.append(element);
+        container.classList.add('show-container');
+    }
+    modal.classList.remove('open-modal');
+}
+
 /* EVENT LISTENERS */
 /* ==================================================================================================== */
 
 taskBtn.addEventListener('click', () => modal.classList.add('open-modal'));
 closeBtn.addEventListener('click', () => modal.classList.remove('open-modal'));
 formCloseBtn.addEventListener('click', () => modal.classList.remove('open-modal'));
+form.addEventListener('submit', submitForm);
 
 /* END */
 /* ==================================================================================================== */
