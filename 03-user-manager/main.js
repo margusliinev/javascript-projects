e9340382069276ee08e226e2defa8c9c64befcb2
Margus Liinev <margus.liinev@gmail.com>
2022-12-14 02:16:12 +0200

    open/close form, get element function

--- 03-user-manager/main.js
+++ 03-user-manager/main.js
@@ -1,14 +1,32 @@
 /* SELECT ITEMS */
 /* ==================================================================================================== */
 
+const addUserBtn = get('.adduser-btn');
+const closeBtn = get('.close-btn');
+const formCloseBtn = get('.form-close-btn');
+const modal = get('.modal');
+
 /* EDIT VARIABLES */
 /* ==================================================================================================== */
 
 /* FUNCTIONS */
 /* ==================================================================================================== */
 
+function get(selection) {
+    const element = document.querySelector(selection);
+    if (element) {
+        return element;
+    } else {
+        throw new Error(`Please check "${selection}" selector, no such element found`);
+    }
+}
+
 /* EVENT LISTENERS */
 /* ==================================================================================================== */
+
+addUserBtn.addEventListener('click', () => modal.classList.add('open-modal'));
+closeBtn.addEventListener('click', () => modal.classList.remove('open-modal'));
+formCloseBtn.addEventListener('click', () => modal.classList.remove('open-modal'));
 
 /* END */
 /* ==================================================================================================== */
