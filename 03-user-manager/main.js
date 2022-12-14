59afeb09ce7751b067ed94fa8535b6d859690090
Margus Liinev <margus.liinev@gmail.com>
2022-12-14 18:32:41 +0200

    submitform and create user, set form back to default

--- 03-user-manager/main.js
+++ 03-user-manager/main.js
@@ -5,9 +5,17 @@ const addUserBtn = get('.adduser-btn');
 const closeBtn = get('.close-btn');
 const formCloseBtn = get('.form-close-btn');
 const modal = get('.modal');
+const form = get('.form');
+const usernameInput = get('#username');
+const passwordInput = get('#password');
+const confirmPasswordInput = get('#confirm-password');
+const emailInput = get('#email');
+const list = get('.users-list');
 
 /* EDIT VARIABLES */
 /* ==================================================================================================== */
+
+let counter = 0;
 
 /* FUNCTIONS */
 /* ==================================================================================================== */
@@ -21,12 +29,57 @@ function get(selection) {
     }
 }
 
+function submitForm(e) {
+    e.preventDefault();
+    counter++;
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+    const email = emailInput.value;
+    const element = document.createElement('article');
+    element.classList.add('user');
+    element.innerHTML = `<div class="column-container">
+                            <p class="number">${counter}</p>
+                         </div>
+                         <div class="column-container">
+                            <p class="username">${username}</p>
+                         </div>
+                         <div class="column-container">
+                            <p class="password">${password}</p>
+                         </div>
+                         <div class="column-container">
+                            <p class="email">${email}</p>
+                         </div>
+                         <div class="column-container">
+                            <button type="button" class="edit-btn">
+                                <i class="fas fa-edit"></i>
+                            </button>
+                         </div>
+                         <div class="column-container">
+                            <button type="button" class="delete-btn">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                         </div>
+                        `;
+    list.append(element);
+    modal.classList.remove('open-modal');
+    setBackToDefault();
+}
+
+function setBackToDefault() {
+    usernameInput.value = '';
+    passwordInput.value = '';
+    confirmPasswordInput.value = '';
+    emailInput.value = '';
+}
+
 /* EVENT LISTENERS */
 /* ==================================================================================================== */
 
 addUserBtn.addEventListener('click', () => modal.classList.add('open-modal'));
 closeBtn.addEventListener('click', () => modal.classList.remove('open-modal'));
 formCloseBtn.addEventListener('click', () => modal.classList.remove('open-modal'));
+form.addEventListener('submit', submitForm);
 
 /* END */
 /* ==================================================================================================== */
