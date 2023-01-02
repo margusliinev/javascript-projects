8459d76f8e28f37bec4d1e2dd528a3afc4d3568e
Margus Liinev <margus.liinev@gmail.com>
2023-01-02 22:24:47 +0200

    combined filtering fix

--- 05-hungry-bear/scripts/filterProducts.js
+++ 05-hungry-bear/scripts/filterProducts.js
@@ -3,29 +3,59 @@ import { menu } from './data.js';
 import { displayProducts } from './displayProducts.js';
 
 const filterBtns = [...document.querySelectorAll('.filter-btn')];
-const form = get('.input-form');
 const searchInput = get('.search-input');
+const form = get('.input-form');
 
-function filterButtons(button) {
-    const newMenu = menu.filter(function (item) {
-        if (item.category === button.textContent) {
+function filterButtons(data, button) {
+    const newMenu = data.filter(function (item) {
+        if (item.category === button.textContent || button.textContent === 'all') {
             return item;
         }
     });
     return newMenu;
 }
 
-function filterProducts(buttonFilterMenu) {
-    displayProducts(buttonFilterMenu);
-    form.addEventListener('keyup', function () {
-        const value = searchInput.value;
-        const newMenu = buttonFilterMenu.filter(function (item) {
-            if (item.title.includes(value)) {
-                return item;
-            }
+function filterSearch(data) {
+    const value = searchInput.value;
+    const newMenu = data.filter(function (item) {
+        if (item.title.includes(value)) {
+            return item;
+        }
+    });
+    return newMenu;
+}
+
+function filterProducts() {
+    filterBtns.forEach((btn) => {
+        btn.addEventListener('click', function (e) {
+            const filteredMenu = filterButtons(menu, e.currentTarget);
+            displayProducts(filteredMenu);
+            form.addEventListener('keyup', function () {
+                const value = searchInput.value;
+                const newMenu = filteredMenu.filter(function (item) {
+                    if (item.title.includes(value)) {
+                        return item;
+                    }
+                });
+                displayProducts(newMenu);
+            });
         });
-        displayProducts(newMenu);
+    });
+
+    form.addEventListener('keyup', function () {
+        const filteredMenu = filterSearch(menu);
+        displayProducts(filteredMenu);
+        filterBtns.forEach((btn) => {
+            btn.addEventListener('click', function () {
+                const newMenu = filteredMenu.filter(function (item) {
+                    if (item.category === btn.textContent || btn.textContent === 'all') {
+                        return item;
+                    }
+                });
+                displayProducts(newMenu);
+            });
+        });
     });
 }
 
-export { filterProducts, filterButtons, filterBtns };
+export { filterProducts };
--- 05-hungry-bear/scripts/pages/index.js
+++ 05-hungry-bear/scripts/pages/index.js
@@ -1,17 +1,8 @@
 import '../navigation.js';
 import '../displayProducts.js';
 import { menu } from '../data.js';
-import { filterProducts, filterButtons, filterBtns } from '../filterProducts.js';
+import { filterProducts } from '../filterProducts.js';
+import { displayProducts } from '../displayProducts.js';
 
-filterProducts(menu);
-
-filterBtns.forEach((btn) => {
-    btn.addEventListener('click', function (e) {
-        if (e.currentTarget.textContent === 'all') {
-            filterProducts(menu);
-        } else {
-            const filteredMenu = filterButtons(e.currentTarget);
-            filterProducts(filteredMenu);
-        }
-    });
-});
+displayProducts(menu);
+filterProducts();
