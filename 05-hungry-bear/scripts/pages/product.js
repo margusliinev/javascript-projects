598ce0bd90227951bce400b7279ca4d3eab2875b
Margus Liinev <margus.liinev@gmail.com>
2023-01-04 03:45:54 +0200

    load single product

--- 05-hungry-bear/scripts/pages/product.js
+++ 05-hungry-bear/scripts/pages/product.js
@@ -1,6 +1,26 @@
 import '../navigation.js';
+import { get } from '../utils.js';
+import { menu } from '../data.js';
+
+const banner = get('.product-banner h3');
+const image = get('.single-product-img');
+const description = get('.description-text');
+const price = get('.price-title span');
+const addToCartBtn = get('.add-to-cart-btn');
+
+let productID;
 
 window.addEventListener('DOMContentLoaded', function () {
-    const urlID = window.location.search;
-    console.log(urlID);
+    const urlID = window.location.search.substring(4);
+    productID = urlID;
+    let meal = menu.find((meal) => meal.id === parseInt(urlID));
+    document.title = `Hungry Bear || ${meal.title}`;
+    banner.textContent = meal.title;
+    image.src = meal.img;
+    description.textContent = meal.desc;
+    price.textContent = `$${meal.price}`;
+});
+
+addToCartBtn.addEventListener('click', function () {
+    // addToCart(productID)
 });
