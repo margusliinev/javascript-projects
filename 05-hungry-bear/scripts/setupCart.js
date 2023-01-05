3695929128b2db3f48047ba42f69bb87828e4175
Margus Liinev <margus.liinev@gmail.com>
2023-01-05 23:54:22 +0200

    add to cart DOM setup

--- 05-hungry-bear/scripts/setupCart.js
+++ 05-hungry-bear/scripts/setupCart.js
@@ -1,7 +1,8 @@
-import { getStorageItem, setStorageItem } from './utils.js';
+import { get, getStorageItem, setStorageItem } from './utils.js';
 import { menu } from './data.js';
 
 let cart = getStorageItem('cart');
+const cartItems = get('.cart-items');
 
 function setupCart() {
     setStorageItem('cart', cart);
@@ -18,6 +19,7 @@ function addToCart(id) {
         let product = findProduct(id);
         product = { ...product, amount: 1 };
         cart = [...cart, product];
+        addToCartDOM(product);
     } else {
         const amount = increaseAmount(id);
         product.amount = amount;
@@ -37,4 +39,35 @@ function increaseAmount(id) {
     return newAmount;
 }
 
-export { setupCart, addToCart };
+function addToCartDOM(product) {
+    const { id, title, img, price, amount } = product;
+    const article = document.createElement('article');
+    article.classList.add('cart-item');
+    article.setAttribute('data-id', id);
+    article.innerHTML = `<img src="${img}" class="cart-item-img" alt="${title}" />
+                        <div>
+                            <h4 class="cart-item-name">${title}</h4>
+                            <p class="cart-item-price">$${price}</p>
+                            <button class="cart-item-remove-btn" data-id="${id}">remove</button>
+                        </div>
+                        <div>
+                            <button class="cart-item-increase-btn" data-id="${id}">
+                                <i class="fas fa-chevron-up"></i>
+                            </button>
+                            <p class="cart-item-amount" data-id="${id}">${amount}</p>
+                            <button class="cart-item-decrease-btn" data-id="${id}">
+                                <i class="fas fa-chevron-down"></i>
+                            </button>
+                        </div>`;
+    cartItems.append(article);
+}
+
+function displayAllCartItems() {
+    cart.forEach((cartItem) => {
+        addToCartDOM(cartItem);
+    });
+}
+
+displayAllCartItems();
+
+export { setupCart, addToCart, addToCartDOM };
